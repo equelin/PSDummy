75da75e0f6ed5bc2d465dcffd4fd2e2a0dc42c2e
equelin <erwan.quelin@gmail.com>
2016-11-19 14:42:21 +0100

    Test new appveyor.yml v7

--- AppVeyor/install.ps1
+++ AppVeyor/install.ps1
@@ -8,35 +8,35 @@ Foreach($import in $Functions)
 {
     Try
     {
-        Write-Host "[$env:BHBuildSystem]-[$env:BHProjectName] - Import function: $($import.fullname)" -ForegroundColor Blue
+        Write-Host "Import function: $($import.fullname)" -ForegroundColor Blue
         . $import.fullname
     }
     Catch
     {
-        Write-Error -Message "Failed to import file $($import.fullname): $_"
+        Throw "Failed to import file $($import.fullname): $_"
     }
 }
 
 # Grab nuget bits, install modules, set build variables, start build.
-Write-Host "[$env:BHBuildSystem]-[$env:BHProjectName] - Get NuGEt package provider" -ForegroundColor Blue
+Write-Host "Get NuGEt package provider" -ForegroundColor Blue
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
-Write-Host "[$env:BHBuildSystem]-[$env:BHProjectName] - Import modules" -ForegroundColor Blue
+Write-Host "Import modules" -ForegroundColor Blue
 Resolve-Module PSDeploy, Pester, BuildHelpers, Format-Pester
 
-Write-Host "[$env:BHBuildSystem]-[$env:BHProjectName] - Set build environment variables" -ForegroundColor Blue
+Write-Host "Set build environment variables" -ForegroundColor Blue
 Set-BuildEnvironment
 
 #Get module version
-Write-Host "[$env:BHBuildSystem]-[$env:BHProjectName] - Get module version" -ForegroundColor Blue
+Write-Host "Get module version" -ForegroundColor Blue
 $ENV:BHModuleVersion = (Test-ModuleManifest -Path $ENV:BHPSModuleManifest).version
 
 #Get PSGallery latest module version
-Write-Host "[$env:BHBuildSystem]-[$env:BHProjectName] - Get PSGallery latest module version" -ForegroundColor Blue
+Write-Host "Get PSGallery latest module version" -ForegroundColor Blue
 $ENV:BHPSGalleryLatestModuleVersion = (Find-Module -Name $ENV:BHProjectName).version
 
 #Get GitHub latest release
-Write-Host "[$env:BHBuildSystem]-[$env:BHProjectName] - Get GitHub latest release" -ForegroundColor Blue
+Write-Host "Get GitHub latest release" -ForegroundColor Blue
 $Releases = Get-GitHubRelease -username 'equelin' -repository $ENV:BHProjectName 
 
 If ($Releases) {
