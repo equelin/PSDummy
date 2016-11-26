59a7f859ea24c4a669df3a1b6b07c8fb5945824d
equelin <erwan.quelin@gmail.com>
2016-11-26 09:59:25 +0100

    Cleaning code

--- AppVeyor/install.ps1
+++ AppVeyor/install.ps1
@@ -2,30 +2,33 @@
 Add-AppVeyorLog -Message 'Get NuGEt package provider' -Category 'Information'
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
+# Import module from PSGallery
 Add-AppVeyorLog -Message 'Import modules from PSGallery' -Category 'Information' -Details 'Module list: PSDeploy, Pester, BuildHelpers, Format-Pester'
 Resolve-Module PSDeploy, Pester, BuildHelpers, Format-Pester
 
+# Set Build environment variables, get them from BuildHelpers module
 Add-AppVeyorLog -Message 'Set build environment variables' -Category 'Information'
 Set-BuildEnvironment
 
-#Get module version
+# Get module version
 Add-AppVeyorLog -Message 'Get module version' -Category 'Information'
 $ENV:BHModuleVersion = (Test-ModuleManifest -Path $ENV:BHPSModuleManifest).version
 
-#Get PSGallery latest module version
+# Get PSGallery latest published module version
 Add-AppVeyorLog -Message 'Get PSGallery latest module version' -Category 'Information'
 $ENV:BHPSGalleryLatestModuleVersion = (Find-Module -Name $ENV:BHProjectName).version
 
-#Get GitHub latest release
+# Get GitHub latest published release
 Add-AppVeyorLog -Message 'Get GitHub latest release' -Category 'Information'
 $Releases = Get-GitHubRelease -username 'equelin' -repository $ENV:BHProjectName 
 
+# Tests if there is an existing release on GitHub, if not set the latest release version to 0.0.0
 If ($Releases) {
     $ENV:BHGitHubLatestReleaseVersion = [version]($releases.tag_name | Select-Object -First 1)
 } else {
     $ENV:BHGitHubLatestReleaseVersion = '0.0.0'
 }
 
-$Items = Get-Item ENV:BH*
+# Show environment variables on the AppVeyor console - TODO: Show these informations on the AppVeyor's messages page
+Get-Item ENV:BH*
 
-Add-AppVeyorLog -Message 'Build environnement variables' -Category 'Information' -Details $Items
--- AppVeyor/test.ps1
+++ AppVeyor/test.ps1
@@ -1,5 +1,3 @@
-Write-Host "***** TEST *****" -ForegroundColor Yellow
-
 # Find the build folder based on build system
 $ProjectRoot = $ENV:BHProjectPath
 
@@ -7,32 +5,30 @@ if(-not $ProjectRoot) {
     $ProjectRoot = $PSScriptRoot
 }
 
+# Set some usefull varialbes
 $Timestamp = Get-date -uformat "%Y%m%d-%H%M%S"
 $PSVersion = $PSVersionTable.PSVersion.Major
 $TestFile = "TestResults_PS$PSVersion`_$TimeStamp.xml"
 $BaseFileName = "TestResults_PS$PSVersion`_$TimeStamp"
 
-# Gather test results. Store them in a variable and file
+# Gather test results. Store them in a variable and a NUnitXml file
 Add-AppVeyorLog -Message 'Run pester tests' -Category 'Information'
 $TestResults = Invoke-Pester -Path $ProjectRoot\Tests -PassThru -OutputFormat NUnitXml -OutputFile "$ProjectRoot\AppVeyor\$TestFile"
 
-# Document the pester results
+# Document the pester results using module Format-Pester
 Add-AppVeyorLog -Message "Documenting the pester's results with Format-Pester" -Category 'Information'
-$FormatPesterResult = $TestResults | Format-Pester -Format 'Text' -BaseFileName $BaseFileName
+$FormatPesterResultFile = $TestResults | Format-Pester -Format 'Text' -BaseFileName $BaseFileName
 
 # In Appveyor?  Upload our tests and documentation! 
-If($ENV:BHBuildSystem -eq 'AppVeyor')
-{
-    Add-AppVeyorLog -Message 'Uploading tests and documentation on Appveyor' -Category 'Information'
-    
-    #Upload NUnitXml tests results
-    (New-Object 'System.Net.WebClient').UploadFile(
-        "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)",
-        "$ProjectRoot\AppVeyor\$TestFile")
+Add-AppVeyorLog -Message 'Uploading tests and documentation on Appveyor' -Category 'Information'
 
-    #Upload Format-Pester tests results   
-    Push-AppveyorArtifact $FormatPesterResult
-}
+#Upload NUnitXml tests results
+(New-Object 'System.Net.WebClient').UploadFile(
+    "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)",
+    "$ProjectRoot\AppVeyor\$TestFile")
+
+#Upload Format-Pester tests results   
+Push-AppveyorArtifact $FormatPesterResultFile
 
 # Stop the build if a pester test fails 
 If ($TestResults.FailedCount -gt 0) {
